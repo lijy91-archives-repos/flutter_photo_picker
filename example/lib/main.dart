4de78c7f76b84868ba8fd6827375b87f5568e594
JianyingLi <lijy91@foxmail.com>
2020-01-10 16:39:32 +0800

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,10 +16,32 @@ class _MyAppState extends State<MyApp> {
   void _handleClickOpenPicker() async {
     List<PhotoPickerAsset> assets = await PhotoPicker.openPicker(
       mediaType: 'image',
+      multiple: true,
+      limit: 3,
       selectedAssets: _selectedAssets,
+      messages: {
+        "Ok": "确定",
+        "Exceed Maximum Number Of Selection": "超过最大选择数",
+        "Denied albums permissions granted": "已拒绝授予相册权限",
+        "Denied camera permissions granted": "已拒绝授予相机权限",
+        "Waiting image/video download to complete": "等待图像/视频下载完成",
+      },
+      options: {
+        'tapHereToChange': '点击此处以更改',
+        'cancelTitle': '取消',
+        'doneTitle': '完成',
+        'emptyMessage': '没有相册',
+        'customLocalizedTitle': {
+          'Camera Roll': '相机相册',
+          'Recents': '最近使用',
+          'Selfies': '自拍',
+          'Videos': '视频'
+        }
+      }
     );
 
     setState(() {
+      print(assets);
       _selectedAssets = assets;
     });
   }
